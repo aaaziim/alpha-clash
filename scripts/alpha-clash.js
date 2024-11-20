5afa9a53b6a8f16429581c0b9011e3e1feae73f6
aaaziim <azim210215@gmail.com>
2024-11-21 00:33:12 +0600

    Functions Structured and Random Alphabet Genarating

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -2,4 +2,14 @@ function play() {
     hideScreenById('home-screen');
     hideScreenById('final-score');
     showScreenById('play-ground');
+    const currentAlphabet = getARandomAlphabet();
+    updateTextById('current-alphabet', currentAlphabet)
+    // console.log(currentAlphabet)
+
 }
+
+function handleKeyUp(event) {
+
+}
+
+document.addEventListener('keyup', handleKeyUp);
--- scripts/utility.js
+++ scripts/utility.js
@@ -4,3 +4,35 @@ function hideScreenById(elementID) {
 function showScreenById(elementID) {
     document.getElementById(elementID).classList.remove('hidden')
 }
+
+
+function getTextById(elementID) {
+    return document.getElementById(elementID).innerText;
+}
+
+function updateTextById(elementID, updatedValue) {
+    document.getElementById(elementID).innerText = updatedValue;
+}
+
+function convertToNumber(textString) {
+    return parseInt(textString)
+}
+
+function setBackgroundColorById(elementId) {
+    const element = document.getElementById(elementId);
+    element.classList.add('bg-orange-400');
+}
+
+function removeBackgroundColorById(elementId) {
+    const element = document.getElementById(elementId);
+    element.classList.remove('bg-orange-400');
+}
+
+
+function getARandomAlphabet() {
+    const alphabetString = "abcdefghijklmnopqrstuvwxyz"
+    const alphabetArray = alphabetString.split("")
+    const index = Math.round(Math.random() * 25)
+    const currentAlphabet = alphabetArray[index];
+    return currentAlphabet;
+}
