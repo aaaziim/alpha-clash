965c01e713189783ff0d4a4262fef297597e2119
aaaziim <azim210215@gmail.com>
2024-11-21 01:09:10 +0600

    Comple Functional Game with Life and Score Calculation

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -2,14 +2,57 @@ function play() {
     hideScreenById('home-screen');
     hideScreenById('final-score');
     showScreenById('play-ground');
-    const currentAlphabet = getARandomAlphabet();
-    updateTextById('current-alphabet', currentAlphabet)
-    // console.log(currentAlphabet)
+
+    updateTextById('current-life', 5);
+    updateTextById('current-score', 0);
+
+
+    continueGame();
 
 }
+function continueGame() {
+    const currentAlphabet = getARandomAlphabet();
+    updateTextById('current-alphabet', currentAlphabet);
+    setBackgroundColorById(currentAlphabet)
+}
 
+function gameOver() {
+    hideScreenById('home-screen');
+    hideScreenById('play-ground');
+    showScreenById('final-score');
+
+}
 function handleKeyUp(event) {
+    const playerPressed = event.key.toLowerCase();
+    const keyToPress = getTextById('current-alphabet').toLowerCase();
 
+    const scoreText = getTextById('current-score');
+    let score = convertToNumber(scoreText);
+    const lifeText = getTextById('current-life');
+    let life = convertToNumber(lifeText);
+
+    if (event.key === 'Escape') {
+        updateTextById('last-score', score);
+        removeBackgroundColorById(keyToPress)
+        gameOver();
+    }
+
+    if (event.key === keyToPress) {
+        score += 1;
+        updateTextById('current-score', score);
+        removeBackgroundColorById(keyToPress);
+        continueGame();
+    } else {
+        life -= 1;
+        updateTextById('current-life', life);
+
+        if (life === 0) {
+            updateTextById('last-score', score);
+            removeBackgroundColorById(keyToPress);
+            gameOver()
+        }
+
+    }
 }
 
 document.addEventListener('keyup', handleKeyUp);
